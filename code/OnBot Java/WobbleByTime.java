65d902e1ccd5df174ec4898c8c41e0f7ddf008d3
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2021-01-07 21:40:52 +0000

    Update and rename Debug.java to WobbleByTime.java

---
+++ code/OnBot Java/WobbleByTime.java
@@ -0,0 +1,94 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@Autonomous(name="WobbleByTime")
+public class WobbleByTime extends LinearOpMode {
+
+    // Declare OpMode members.
+    // private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor topLeftDrive = null;
+    private DcMotor topRightDrive = null;
+    private DcMotor bottomLeftDrive = null;
+    private DcMotor bottomRightDrive = null;
+
+    static final int MOTOR_TICK_COUNT = 1120;
+    static final int GEAR_RATIO = 80 / 40;
+    static final double CIRCUMFERENCE = 12.1208661;
+
+    static final int TICKS_PER_WHEEL_ROTATION = (int) (MOTOR_TICK_COUNT / GEAR_RATIO);
+    // static final double AVG_MARGIN_OF_ERROR = null;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables.
+        topLeftDrive  = hardwareMap.get(DcMotor.class, "front_left_motor");
+        topRightDrive = hardwareMap.get(DcMotor.class, "front_right_motor");
+        bottomLeftDrive  = hardwareMap.get(DcMotor.class, "back_left_motor");
+        bottomRightDrive = hardwareMap.get(DcMotor.class, "back_right_motor");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        topLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        bottomLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        topRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        bottomRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        // runtime.reset();
+
+        // Run
+        // Sleep for a minute to add half a second of stress for everyone on the team
+        sleep(500);
+        
+        topLeftDrive.setPower(1);
+        topRightDrive.setPower(1);
+        bottomLeftDrive.setPower(1);
+        bottomRightDrive.setPower(1);
+        
+        sleep(100);
+        
+        topLeftDrive.setPower(1);
+        topRightDrive.setPower(-1);
+        bottomLeftDrive.setPower(-1);
+        bottomRightDrive.setPower(1);
+        
+        sleep(300);
+
+        topLeftDrive.setPower(1);
+        topRightDrive.setPower(1);
+        bottomLeftDrive.setPower(1);
+        bottomRightDrive.setPower(1);
+        
+        sleep(900);
+        
+        topLeftDrive.setPower(0);
+        topRightDrive.setPower(0);
+        bottomLeftDrive.setPower(0);
+        bottomRightDrive.setPower(0);
+        
+        sleep(900);
+        
+        topLeftDrive.setPower(-1);
+        topRightDrive.setPower(-1);
+        bottomLeftDrive.setPower(-1);
+        bottomRightDrive.setPower(-1);
+        
+        sleep(150);
+        
+        topLeftDrive.setPower(0);
+        topRightDrive.setPower(0);
+        bottomLeftDrive.setPower(0);
+        bottomRightDrive.setPower(0);
+        
+    }
+}
